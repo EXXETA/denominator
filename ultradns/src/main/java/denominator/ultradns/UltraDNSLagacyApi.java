66bc92b33a87cebb2c0bd8757669068222ae8bae
Thomas Pohl <thomas.pohl@exxeta.com>
2016-06-06 13:53:51 +0200

    Fix updates of webforwards

--- ultradns/src/main/java/denominator/ultradns/UltraDNSLagacyApi.java
+++ ultradns/src/main/java/denominator/ultradns/UltraDNSLagacyApi.java
@@ -14,37 +14,44 @@ public final class UltraDNSLagacyApi implements LagacyApi {
 	private final String zoneName;
 
 	@Inject
-	UltraDNSLagacyApi(UltraDNS api, String zoneName){
+	UltraDNSLagacyApi(UltraDNS api, String zoneName) {
 		this.api = api;
 		this.zoneName = zoneName;
 	}
 
 	public void putWebForward(WebForward webForward) {
-		if (webForward.guid()==null){
-			api.addWebForward(zoneName, webForward.requestTo(), webForward.redirectTo(), webForward.forwardType());
-		}else{
-			api.updateWebForward(webForward.guid(), webForward.requestTo(), webForward.redirectTo(), webForward.forwardType());
+		WebForward existingWebForward = null;
+		for(WebForward wf : webForwards()){
+			if (wf.requestTo().equals(webForward.requestTo())){
+				existingWebForward=wf;
+			}
 		}
+		if (existingWebForward==null){
+			api.addWebForward(zoneName, webForward.requestTo(), webForward.redirectTo(), webForward.forwardType());
+		}else {
+			api.updateWebForward(existingWebForward.guid(), webForward.requestTo(), webForward.redirectTo(),
+					webForward.forwardType());
+		}
+
 	}
 
 	public Iterator<WebForward> getWebForwards() {
 		Iterator<WebForward> forwards = webForwards().iterator();
 		return forwards;
 	}
-	
-	private List<WebForward> webForwards(){
+
+	private List<WebForward> webForwards() {
 		return api.queryWebForwards(zoneName);
 	}
-	
-	public void deleteWebForwardByRequestTo(String requestTo){
-		for (WebForward wf : webForwards()){
-			if (wf.requestTo().equals(requestTo)){
+
+	public void deleteWebForwardByRequestTo(String requestTo) {
+		for (WebForward wf : webForwards()) {
+			if (wf.requestTo().equals(requestTo)) {
 				api.deleteWebForward(wf.guid());
 			}
 		}
 	}
 
-	
 	static final class Factory implements denominator.LagacyApi.Factory {
 
 		private final UltraDNS api;
